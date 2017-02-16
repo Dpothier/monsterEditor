d05623e93eb2149e9856f97f7db55a1f0742371a
Dpothier <dpothier@live.ca>
2017-02-16 16:33:24 -0500

    Compute global CR based on offensive and defensive CR. Rounding CR based on possible CR (including fractionnal ones)

--- src/components/monsterEditor/ChallengeRatingComponent.js
+++ src/components/monsterEditor/ChallengeRatingComponent.js
@@ -18,21 +18,41 @@ class ChallengeRating extends React.Component {
   }
 
   changeOffensiveCr(event){
-    this.setState({offensiveCr: event.target.value});
-    this.calculateAverageCr();
+    this.updateCr(Number(event.target.value), this.state.defensiveCr);
   }
 
   changeDefensiveCr(event){
-    this.setState({defensiveCr: event.target.value});
-    this.calculateAverageCr();
+    this.updateCr(this.state.offensiveCr, Number(event.target.value));
+  }
+
+  updateCr(offensiveCr, defensiveCr){
+    var averageCr = (offensiveCr + defensiveCr) / 2;
+    averageCr = this.RoundToValidCr(averageCr);
+
+    this.setState({
+      offensiveCr : offensiveCr,
+      defensiveCr : defensiveCr,
+      effectiveCr : averageCr
+    })
+  }
+
+  RoundToValidCr(cr){
+    var validCr = [0, 1/8, 1/4, 1/2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+    let i;
+    for(i = 0; i < validCr.length - 1; i++){
+      const roundingPoint = (validCr[i] + validCr[i + 1]) / 2;
+      if(cr < roundingPoint)
+        return validCr[i];
+    }
+    return validCr[validCr.length]; //If no other CR fits, uses biggest
   }
 
   render() {
     return (
       <div className="challengerating-component">
-        <span> Offensive Challenge Rating <input type="text" value={this.state.offensiveCr} onChange={this.changeOffensiveCr}/> </span>
-        <span> Defensive Challenge Rating <input type="text" value={this.state.defensiveCr} onChange={this.changeDefensiveCr}/> </span>
-        <span> Effective Challenge Rating <input type="text" value={this.state.effectiveCr} disabled/>
+        <span> Offensive Challenge Rating <input type="number" step="0.1" value={this.state.offensiveCr} onChange={this.changeOffensiveCr}/> </span>
+        <span> Defensive Challenge Rating <input type="number" step="0.1" value={this.state.defensiveCr} onChange={this.changeDefensiveCr}/> </span>
+        <span> Effective Challenge Rating <input type="number" step="0.1" value={this.state.effectiveCr} disabled/> </span>
       </div>
     );
   }
